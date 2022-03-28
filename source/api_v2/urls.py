7438cb6e08e2759d90d66fc3d19ba3d8b182eb6b
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-03-28 19:14:17 +0600

    поменял путь создания!

--- source/api_v2/urls.py
+++ source/api_v2/urls.py
@@ -10,7 +10,7 @@ app_name = 'api_v2'
 
 urlpatterns = [
     path("articles/", ArticleListView.as_view(), name='article'),
-    path("articles/", ArticleCreateView.as_view(), name='article_create'),
+    path("articles/create/", ArticleCreateView.as_view(), name='article_create'),
     path("articles/<int:pk>/", ArticleDetailView.as_view(), name='article_detail'),
     path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name='article_update'),
     path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name='article_delete'),
